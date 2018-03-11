572f4da05744ac693e37bc41d645a2092a2387b4
mzh <ma.hed@bluewin.ch>
2018-03-11 21:49:29 +0100

    Questions

--- app.js
+++ app.js
@@ -6,18 +6,34 @@ app.controller("ctl", function($scope) {
                      [2, "Question 2"],
                      [3, "Question 3"]]
 
-
     // Initial questionId and text.
     var questionId = 0;
     $scope.question = questions[questionId][1];
+    $scope.questionCounter = (questionId+1)+"/"+questions.length;
 
     $scope.last = function() {
-        questionId = questionId - 1;
-        $scope.question = questions[questionId][1];
+        if(questionId > 0) {
+            questionId = questionId - 1;
+            $scope.question = questions[questionId][1];
+        } else {
+            console.log("First question.");
+        }
+        updateQuestionCounter(questionId);
     }
 
     $scope.next = function() {
-        questionId = questionId + 1;
-        $scope.question = questions[questionId][1];
+        if(questionId < questions.length-1) {
+            questionId = questionId + 1;
+            $scope.question = questions[questionId][1];
+        } else {
+            console.log("Last question.");
+        }
+        updateQuestionCounter(questionId);
+    }
+
+    function updateQuestionCounter(id) {
+        var questionString = (id+1) + "/" + questions.length;
+        $scope.questionCounter = questionString;
+        //console.log(id);
     }
 });
