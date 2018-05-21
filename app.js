7ffd1ea22c7b111351ae180e14fa6c27a4f688fc
mzh <ma.hed@bluewin.ch>
2018-05-21 11:39:28 +0200

    Working prototype of summary function

--- app.js
+++ app.js
@@ -1,39 +1,240 @@
+// Define application.
 var app = angular.module("app", []);
 
-app.controller("ctl", function($scope) {
+// Define the controller of the application.
+app.controller("QuestionsController", function($scope) {
 
-    var questions = [[1, "Question 1"],
-                     [2, "Question 2"],
-                     [3, "Question 3"]]
+    /* --------------------------------------------------------- */
+    /* Initialization.                                           */
+    /* --------------------------------------------------------- */
+    /* Note: The sequence of 'questions' and the sequence of
+    *        'questionsTypes' is critically important. They
+    *        have to be in agreement for the calculation to
+    *        correctly work.                                     
+    /* --------------------------------------------------------- */
+    var questions      = ["Question 1 text.", "Question 2 text.",
+                          "Question 3 text.", "Question 4 text.",
+                          "Question 5 text.", "Question 6 text."];
+    var questionsTypes = ["Drama-guy", "Initiative", "Spontaneous",
+                          "Drama-guy", "Initiative", "Spontaneous"];
+    var classes        = ["Initiative",  "Drama-guy", "Spontaneous"];
 
-    // Initial questionId and text.
-    var questionId = 0;
-    $scope.question = questions[questionId][1];
+    // Initial values.
+    var answers        = ["Maybe", "Maybe", "Maybe",
+                          "Maybe", "Maybe", "Maybe"];
+
+    // "Class A", "Class B", "Class C"
+    //var summaryByClass = [0, 0, 0];
+    var summaryByClass = [];
+
+    // Initial questionId, text and answers.
+    var questionId         = 0;
+    $scope.question        = questions[questionId];
     $scope.questionCounter = (questionId+1)+"/"+questions.length;
+    $scope.answer          = answers[questionId];
+    summarizeByClass();
 
-    $scope.last = function() {
-        if(questionId > 0) {
-            questionId = questionId - 1;
-            $scope.question = questions[questionId][1];
-        } else {
-            console.log("First question.");
-        }
-        updateQuestionCounter(questionId);
+    // Hide 'results' initially.
+    // var showResults = true;
+    // document.getElementById("resultsDraw").style.display = "block";
+
+    var svg = d3.select("#resultsDraw").append("svg")
+              .attr("height", "100%")
+              .attr("width",  "100%");
+
+    console.log("Initialization: ");
+    draw(summaryByClass);
+    /* --------------------------------------------------------- */
+
+
+    /* ----------------------------------------------------------*/
+    /*  Visualization.                                           */
+    /* ----------------------------------------------------------*/
+    /* Search for array of rectangles,
+    *  'selectAll' for array,
+    *  'enter().append' to append rectangles,
+    *  'attr' add attributes for rectangles.
+    *  
+    *  'x': distance from left side of browser in px
+    *  'y': Distance from top of browser
+    *  'height': Height of the rectangle
+    * 
+    *  d: datapoint
+    *  i: index
+    *  To reposition the rectangles, we’ll modify
+    *  the y attribute to subtract the space on top. 
+    *
+    *  NOTE:
+    *  The minimal value of 'width' is '+10' to provide visual
+    *  cue also when computed value is zero.                     */
+    /* ----------------------------------------------------------*/
+    function draw(values) {
+        console.log("draw");
+        console.log(values);
+
+        // Remove 'rect' from previous 'draw' call.
+        d3.selectAll("rect").remove();
+
+        svg.selectAll("rect")
+        .data(values).enter().append("rect")
+        .attr("height", function(d, i) {return 10})
+        .attr("width", function(d, i) {return d*20+10})
+        .attr("x", function(d, i) {return 0})
+        .attr("y", function(d, i) {return (i*30)+5});
+    }
+    /* ----------------------------------------------------------*/
+
+
+    /* ----------------------------------------------------------*/
+    /* Mechanics.                                                */
+    /* ----------------------------------------------------------*/
+    $scope.getResults = function() {
+        // console.log(answers);
+        summarizeByClass();
+        console.log("getResults: ");
+        console.log(summaryByClass);
+        draw(summaryByClass);
+
+        // Toggle 'results' and 'questions' display.
+        // Show results.
+        // if(showResults==false) {
+        //     document.getElementById("main").style.display = "none";
+        //     document.getElementById("resultsDraw").style.display = "block";
+        //     showResults = true;
+
+        // // Show questions.
+        // } else {
+        //     document.getElementById("main").style.display = "block";
+        //     document.getElementById("resultsDraw").style.display = "none";
+        //     showResults = false;
+        // } 
     }
 
-    $scope.next = function() {
+    function summarizeByClass() {
+        summaryByClass = [0, 0, 0];
+        console.log("summarizeByClass: ", summaryByClass);
+
+        for(var i=0; i<classes.length; i++) {
+            indecesOfClass = getIndecesOfClass(i);
+
+            for(var j=0; j<indecesOfClass.length; j++) {
+                if(answers[indecesOfClass[j]] == "No") {
+                    summaryByClass[i] += 0;
+                }
+                if(answers[indecesOfClass[j]] == "Yes") {
+                    summaryByClass[i] += 2
+                }
+                if(answers[indecesOfClass[j]] == "Maybe") {
+                summaryByClass[i] += 1;
+                }
+            }
+        }
+        $scope.results = classes[0] + ": " + summaryByClass[0] + ", " + 
+                         classes[1] + ": " + summaryByClass[1] + ", " +
+                         classes[2] + ": " + summaryByClass[2];
+    }
+
+    function getIndecesOfClass(i) {
+        var indecesOfClass = [];
+        for(var j=0; j<questionsTypes.length; j++) {
+            if(classes[i]==questionsTypes[j]) {
+                indecesOfClass.push(j);
+            }
+        }
+        return indecesOfClass;
+    }
+    /* ----------------------------------------------------------*/
+
+
+    /* ----------------------------------------------------------*/
+    /*  Slide window, side effect depends on trigger event.      */
+    /* ----------------------------------------------------------*/
+    $scope.step = function(event) {
+        var idOfEvent = event.target.id;
+        if(idOfEvent == "last" ) {    stepBack();          }
+        if(idOfEvent == "next" ) { stepForward("Current"); }
+        if(idOfEvent == "no"   ) { stepForward("No"     ); }
+        if(idOfEvent == "maybe") { stepForward("Maybe"  ); }
+        if(idOfEvent == "yes"  ) { stepForward("Yes"    ); }
+    }
+
+    function stepForward(inp) {
+        if(inp=="Current") { answers[questionId] = answers[questionId]; }
+        if(inp=="Maybe")   { answers[questionId] =             "Maybe"; }
+        if(inp=="Yes")     { answers[questionId] =               "Yes"; }
+        if(inp=="No")      { answers[questionId] =                "No"; }
+        makeForwardStep();
+        slideToNextQuestion(questionId);
+    }
+
+    function makeForwardStep() {
         if(questionId < questions.length-1) {
             questionId = questionId + 1;
-            $scope.question = questions[questionId][1];
-        } else {
-            console.log("Last question.");
-        }
-        updateQuestionCounter(questionId);
+        } else { console.log("Last question."); }
     }
 
-    function updateQuestionCounter(id) {
+    function stepBack() {
+        if(questionId > 0) {
+            questionId = questionId - 1;
+        } else { console.log("First question."); }
+        slideToNextQuestion(questionId);
+    }
+
+    function slideToNextQuestion(id) {
         var questionString = (id+1) + "/" + questions.length;
         $scope.questionCounter = questionString;
-        //console.log(id);
+        $scope.question        = questions[questionId];
+        $scope.answer          = answers[questionId];
     }
+
+    /* Deprecated. 
+    * function summarizeAnswers() {
+    *     // Reset required to prevent cumulation of values.
+    *     summary = [0, 0, 0];
+    *     for(var i=0; i<answers.length; i++) {
+    *         if(answers[i]=="No")    { summary[0] += 1 }
+    *         if(answers[i]=="Yes")   { summary[1] += 1 }
+    *         if(answers[i]=="Maybe") { summary[2] += 1 }
+    *      } 
+    *      console.log(summary);
+    * } */
+    /* ----------------------------------------------------------*/
+
+
+    /* ----------------------------------------------------------*/
+    /* To do / Clearify / Improvements                           */
+    /* ----------------------------------------------------------*/
+    /*
+    *  To do:
+    *  - Count of answers per category
+    *  - Slide transition between questions (CSS or JS animations?)
+    *  - UX response for first / last question, like fade out
+    *    buttons or start at question 1 again
+    *  - Slim out controller
+    *  - Questions and questionsTypes data structure (e.g. csv):
+    *        Question,        Type
+    *        -------------------------------
+    *        Question_Text_1, questionType_1
+    *        Question_Text_2, questionType_2
+    *        ...
+    *
+    *  Clearify:
+    *  - Can the test be finished without answering every question?
+    *
+    *  UX:
+    *  - Should a zero value be visualized?
+    *  - Initial values of questions 'Maybe' or 'None'?
+    *  - How to handle early finish of test? Give results, save, ..?
+    *
+    *  Improvements:
+    *  - Use 'display' or 'visibility' and a list for the questions
+    *  - Export toggling of questions and results display out from
+    *    the results calculation 'getResults'
+    *  - Top and low row of buttons within a div.
+    *  - Answers as radio buttons?
+    *  - Minimal visual value is '+10', should minimal computed value
+    *    be '+10' instead?
+    */
+    /* ----------------------------------------------------------*/
 });
+
